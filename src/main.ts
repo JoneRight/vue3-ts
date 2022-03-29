5491488d83d2f1d13cdde589b674d636c9204ffa
zhangxiangjun <zhangxiangjun@baiyyy.com.cn>
2022-03-29 17:36:36 +0800

    update

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+
+// 使用less
 
 // 再main中全局引入组件
-import Antd from "ant-design-vue";
-import "ant-design-vue/dist/antd.css";
+import Antd from 'ant-design-vue'
+import 'ant-design-vue/dist/antd.less'
+import '@/common/styles/theme.less'
 
-createApp(App).use(store).use(router).use(Antd).mount("#app");
+createApp(App).use(store).use(router).use(Antd).mount('#app')
