2d8c7df12b26e35013c839ae92a40135395bb5b2
zhangxiangjun <zhangxiangjun@baiyyy.com.cn>
2022-03-29 10:17:35 +0800

    update

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
 
-createApp(App).use(store).use(router).mount('#app')
+// 再main中全局引入组件
+import Antd from "ant-design-vue";
+import "ant-design-vue/dist/antd.css";
+
+createApp(App).use(store).use(router).use(Antd).mount("#app");
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    123
+    <a-button type="primary" danger>Primary</a-button>
   </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
 @Options({
   components: {
-    HelloWorld,
   },
 })
 export default class HomeView extends Vue {}
 </script>
+  
